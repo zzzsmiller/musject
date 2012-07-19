5f3fd94fc9cd47a785c4c6bc3f445289b5760b99
Igor M <zzzsmiller@mail.ru>
2012-07-19 11:14:21 +0400

    change GXT version

--- src/main/java/com/smiller/musject/client/form/EditGreetingForm.java
+++ src/main/java/com/smiller/musject/client/form/EditGreetingForm.java
@@ -1,9 +1,7 @@
 package com.smiller.musject.client.form;
 
-import com.extjs.gxt.ui.client.widget.form.FormPanel;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.HTMLPanel;
+import com.sencha.gxt.widget.core.client.TabPanel;
+import com.sencha.gxt.widget.core.client.form.FormPanel;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,13 +11,24 @@ import com.google.gwt.user.client.ui.HTMLPanel;
  * To change this template use File | Settings | File Templates.
  */
 public class EditGreetingForm extends FormPanel {
-    interface EditGreetingFormmUiBinder extends UiBinder<HTMLPanel, EditGreetingForm> {
+
+    /*interface EditGreetingFormmUiBinder extends UiBinder<Widget, EditGreetingForm> {
     }
 
     private static EditGreetingFormmUiBinder ourUiBinder = GWT.create(EditGreetingFormmUiBinder.class);
 
     public EditGreetingForm() {
-        HTMLPanel rootElement = ourUiBinder.createAndBindUi(this);
+        Widget rootElement = ourUiBinder.createAndBindUi(this);
+
+    }*/
+
+    TabPanel tabs;
+
+
+
+    public EditGreetingForm() {
+        tabs = new TabPanel();
+        setWidget(tabs);
 
     }
 }
